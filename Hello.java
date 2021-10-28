2023c8fa7078782dcb8e750621f922c09fe6be03
wenqiuZhou <56452768+wenqiuZhou@users.noreply.github.com>
2021-10-27 21:52:03 -0400

    merger main

--- Hello.java
+++ Hello.java
@@ -1,8 +1,13 @@
 public class Hello{
 public static void main(String[] args){
+System.out.println("Hello world!");
+System.out.println("Add a new line in main branch");
 }
+
+
 public void method2(){
 }
 public void method3(){
 }
 }
+
